887e4023ef9b965faf7b4b68e13aa57275e0f7c6
IvanMerino515 <merdix515@gmail.com>
2024-12-09 17:31:25 +0100

    updtd

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import  Button from './components/button/button';
+import Box from "./components/box/Box";
+import Container from "./components/Container/Container";
 
 const App = () => {
-	return <>
-	<Button text = 'Click 1'/>
-	<Button text = 'Click 2'/>
-	<Button text = 'Click 3'/>
-	</>
+	return (
+		<Container>
+			<Box color='yellow' title='SEDANS' text = 'Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.'	/>
+			<Box color='blue' title='SUVS' text = 'Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.' />
+			<Box color='green' title='LUXURY' text = 'Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style. ' />
+		</Container>
+	);
 };
 
-export default App
+export default App;
--- src/components/button/Button.jsx
+++
@@ -1,7 +0,0 @@
-const Button = (props) => {
-    console.log(props)
-    return <button>{props.text} </button>
-
-}
-
-export default Button
